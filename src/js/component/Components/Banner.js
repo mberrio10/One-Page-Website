c79e5f55879c0b0d5c8ff6554a89bf22fc2d788a
Mike Berrio <mikeberrio10@hotmail.com>
2020-03-05 18:32:15 +0000

    adding navs and banner

--- src/js/component/Components/Banner.js
+++ src/js/component/Components/Banner.js
@@ -1,14 +1,12 @@
 import React from "react";
+import backGround from "../../../img/bg.jpg";
 
 export class Banner extends React.Component {
-    render () {
-        return (
-            <div className="jumbotron jumbotron-fluid">
-                <div className="container">
-                    <h1 className="display-4">Fluid jumbotron</h1>
-                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="jumbotron jumbotron-fluid">
+				<h1 className="display-4">Fluid jumbotron</h1>
+			</div>
+		);
+	}
 }
