937a935a398f31142d97f408075bb32a30c092c8
Mike Berrio <mikeberrio10@hotmail.com>
2020-05-12 15:25:25 +0000

    parallax added

--- src/js/component/Components/Banner.js
+++ src/js/component/Components/Banner.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 
 export class Banner extends React.Component {
 	render() {
 		return (
-			<div className="jumbotron jumbotron-fluid">
-				<h1 className="display-4">
-					Lorem ipsum <br />
-					<span>dolor</span> sit amet
-				</h1>
-			</div>
+			<Parallax
+				blur={{ min: -1, max: 3 }}
+				bgImage={"http://placekitten.com/1500/700"}
+				strength={200}>
+				<div className="jumbotron jumbotron-fluid">
+					<h1 className="display-4">
+						Lorem ipsum <br />
+						<span>dolor</span> sit amet
+					</h1>
+				</div>
+			</Parallax>
 		);
 	}
 }
